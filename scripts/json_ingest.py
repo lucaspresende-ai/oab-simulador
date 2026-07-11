2629dd5f359b774c0910d98e94848de4507eb935
pedro0perry <p283013@dac.unicamp.br>
2026-07-11 15:22:41 -0300

    nao testei

--- scripts/json_ingest.py
+++ scripts/json_ingest.py
@@ -1,5 +1,30 @@
+from pathlib import Path
+import json
+from supabase import create_client
+from dotenv import load_dotenv
+import os
 
-print('teste conflito')
+supabase_key = os.getenv('SUPABASE_API_KEY')
+supabase_url = os.getenv('SUPABASE_URL')
 
-print('Hello WOrld')
+client = create_client(supabase_key, supabase_url)
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+DADOS = BASE_DIR / "dados_brutos"
+
+arquivos = [
+    p for p in DADOS.iterdir() if p.is_file()
+]
+
+for arquivo in arquivos:
+    with open(arquivo, "r", encoding='utf-8') as f:
+        questoes = json.load(f)
+
+    for questao in questoes:
+        questao['alternativa_a'] = questao['alternativas'][0]
+        questao['alternativa_b'] = questao['alternativas'][1]
+        questao['alternativa_c'] = questao['alternativas'][2]
+        questao['alternativa_d'] = questao['alternativas'][3]
+        
+        questao.remove('alternativas')
+    
